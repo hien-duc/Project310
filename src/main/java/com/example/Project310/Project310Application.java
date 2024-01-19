a19659ed529080b307b9f9f01b657870bb981191
Duc <duc.t.cit21@eiu.edu.vn>
2024-01-19 12:57:41 +0700

    change properties

--- src/main/java/com/example/Project310/Project310Application.java
+++ src/main/java/com/example/Project310/Project310Application.java
@@ -1,10 +1,13 @@
 package com.example.Project310;
 
 import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project310Application {
+	private static final Logger logger = LoggerFactory.getLogger(Project310Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project310Application.class, args);
