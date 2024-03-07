dad258a1f9cf95496e89ff911ff94a4ae6851fd8
Duc <duc.t.cit21@eiu.edu.vn>
2024-03-07 14:53:36 +0700

    add security and fetch

--- src/main/java/com/example/Project310/Project310Application.java
+++ src/main/java/com/example/Project310/Project310Application.java
@@ -1,6 +1,7 @@
 package com.example.Project310;
 
 import com.example.Project310.model.*;
+import com.example.Project310.repositories.AppUserRepository;
 import com.example.Project310.repositories.AuthorRepository;
 import com.example.Project310.repositories.BookRepository;
 import com.example.Project310.repositories.MemberRepository;
@@ -14,7 +15,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication()
 @ComponentScan("com.example.Project310.repository")
 public class Project310Application implements CommandLineRunner {
 
@@ -23,17 +24,19 @@ public class Project310Application implements CommandLineRunner {
 	private final AuthorRepository authorRepository;
 	private final MemberRepository memberRepository;
 	private final RentalRepository rentalRepository;
+	private final AppUserRepository urepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project310Application.class, args);
 	}
 
 	public Project310Application(BookRepository bookRepository, AuthorRepository authorRepository,
-			MemberRepository memberRepository, RentalRepository rentalRepository) {
+			MemberRepository memberRepository, RentalRepository rentalRepository, AppUserRepository urepository) {
 		this.bookRepository = bookRepository;
 		this.authorRepository = authorRepository;
 		this.memberRepository = memberRepository;
 		this.rentalRepository = rentalRepository;
+		this.urepository = urepository;
 	}
 
 	@SuppressWarnings("null")
@@ -43,12 +46,11 @@ public class Project310Application implements CommandLineRunner {
 		Author authors2 = new Author("2222", "22222", "minh");
 		authorRepository.saveAll(Arrays.asList(authors, authors2));
 
-//		bookRepository.save(new Book("name", "name", 0, "name", "íbn", authors, new Rental("a", "b", "c", "d")));
-//		bookRepository.save(new Book("name", "name", 0, "name", "íbn", authors, new Rental("a", "b", "c", "d")));
-//		bookRepository.save(new Book("name", "name", 0, "name", "íbn", authors2, null));
-
 		for (Book books : this.bookRepository.findAll()) {
 			logger.info(books.toString());
 		}
+		urepository.save(new AppUser("user", "$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue", "USER"));
+		// Username: admin, password: admin
+		urepository.save(new AppUser("admin", "$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW", "ADMIN"));
 	}
 }
---
+++ src/main/java/com/example/Project310/repositories/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.example.Project310.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.Project310.model.AppUser;
+
+@RepositoryRestResource
+public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+	Optional<AppUser> findByUsername(String username);
+
+}
